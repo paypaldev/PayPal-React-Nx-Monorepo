09ae912ef0d3410d6c1176cca738907772f2f791
Pato <pevargas2730@gmail.com>
2023-10-12 04:17:17 -0400

    react router

--- apps/paypal-store-monorepo/src/app/app.tsx
+++ apps/paypal-store-monorepo/src/app/app.tsx
@@ -1,13 +1,20 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.scss';
+import { Route, Routes } from 'react-router-dom';
+
+// importing the component from the library
+import { ProductList } from '@react-monorepo/products';
 
 import NxWelcome from './nx-welcome';
 
+function Home() {
+  return <h1>Home</h1>;
+}
+
 export function App() {
   return (
-    <div>
-      <NxWelcome title="paypal-store-monorepo" />
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/products" element={<ProductList />}></Route>
+    </Routes>
   );
 }
 
