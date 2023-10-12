271ea92f5fd269403da629955a33170444a985bc
Pato <pevargas2730@gmail.com>
2023-10-12 03:40:27 -0400

    Initial commit

---
+++ apps/paypal-store-monorepo/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.scss';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="paypal-store-monorepo" />
+    </div>
+  );
+}
+
+export default App;
